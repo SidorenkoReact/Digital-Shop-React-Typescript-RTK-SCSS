7f07ce337f9e08ffd647afda22a653ba3f391f47
exess <exess@yandex.ru>
2024-01-03 12:28:51 +1000

    add layout/base scss styles

--- src/components/layouts/Main/content/HistorySlider/HistorySlider.tsx
+++ src/components/layouts/Main/content/HistorySlider/HistorySlider.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const HistorySlider = () => {
     return (
         <div className="main-bottom__history-carousel-holder"
-             style={{display: "flex", justifyContent: "space-around"}}>
+             style={{display: "flex", justifyContent: "space-around", marginTop:24}}>
             <div style={{width: 180, height: 180, backgroundColor: "#b0d5d5"}}>(item 1)</div>
             <div style={{width: 180, height: 180, backgroundColor: "#b0d5d5"}}>(item 2)</div>
             <div style={{width: 180, height: 180, backgroundColor: "#b0d5d5"}}>(item 3)</div>
